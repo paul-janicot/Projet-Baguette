a26206ed037115bfc80aeef3a86e30950f5e2038
Reidrev <baptiste.verdier@artfx.fr>
2025-03-05 14:07:52 +0100

    Trying RandomRoam

--- Assets/Scenes/Baptiste/Scripts/Enemy_Script.cs
+++ Assets/Scenes/Baptiste/Scripts/Enemy_Script.cs
@@ -5,27 +5,47 @@ using UnityEngine.AI;
 
 public class Enemy_Script : MonoBehaviour
 {
-    public Camera cam;
     private NavMeshAgent agent;
+
+    public float range; //radius of sphere
+    private Transform centrePoint; //centre of the area the agent wants to move around in
+    //instead of centrePoint you can set it as the transform of the agent if you don't care about a specific area
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        centrePoint = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) 
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+ 
 
-            if (Physics.Raycast(ray, out hit))
+        if (agent.remainingDistance <= agent.stoppingDistance) //done with path
+        {
+            Vector3 point;
+            if (RandomPoint(centrePoint.position, range, out point)) //pass in our centre point and radius of area
             {
-                agent.SetDestination(hit.point);
+                agent.SetDestination(point);
             }
         }
 
+    }
+    bool RandomPoint(Vector3 center, float range, out Vector3 result)
+    {
+
+        Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1.0f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        {
+            //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
+            //or add a for loop like in the documentation
+            result = hit.position;
+            return true;
+        }
+
+        result = Vector3.zero;
+        return false;
     }
 }
