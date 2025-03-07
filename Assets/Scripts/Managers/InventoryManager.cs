728372f940c2c0c777eba43ad0fb784a6b0c4c66
unknown <joa.bories>
2025-03-07 11:50:13 +0100

    [add] basic inventory and GameManager prefab

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -1,3 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+
+    [SerializeField] private Dictionary<Loot, int> inventoryDict;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public Dictionary<Loot, int> GetInventory()
+    {
+        return inventoryDict;
+    }
+
+    public void AddItem(Loot item)
+    {
+        if (inventoryDict != null)
+        {
+            if (inventoryDict.ContainsKey(item))
+            {
+                inventoryDict[item]++;
+            }
+            else
+            {
+                inventoryDict[item] = 1;
+            }
+        }
+    }
+
+    public bool RemoveItem(Loot item)
+    {
+        if (inventoryDict != null)
+        {
+            if (inventoryDict.ContainsKey(item))
+            {
+                if (inventoryDict[item] > 0)
+                {
+                    inventoryDict[item]--;
+                    return true;
+                }
+            }
+        }
+        Debug.LogWarning("Fools you are trying to take an item that you don't have");
+        return false;
+    }
+
+}
