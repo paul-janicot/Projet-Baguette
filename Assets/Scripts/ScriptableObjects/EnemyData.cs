fa2780a598f94956d3a6566b82997ab8502cb8dc
JoaBories <joa.bories@artfx.fr>
2025-03-21 11:32:30 +0100

    [update] shooting speed on enemyData

--- Assets/Scripts/ScriptableObjects/EnemyData.cs
+++ Assets/Scripts/ScriptableObjects/EnemyData.cs
@@ -7,13 +7,15 @@ public class EnemyData : ScriptableObject
     public float hp;
     public float speed;
     public float shootingRange;
+    public float shootingSpeed;
 
-    public EnemyData(bool isLooting, float hp, float speed, float shootingRange)
+    public EnemyData(bool isLooting, float hp, float speed, float shootingRange, float shootingSpeed)
     {
         this.isDropping = isLooting;
         this.hp = hp;
         this.speed = speed;
         this.shootingRange = shootingRange;
+        this.shootingSpeed = shootingSpeed;
     }
 }
 
