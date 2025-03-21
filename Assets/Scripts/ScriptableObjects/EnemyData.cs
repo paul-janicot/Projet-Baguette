056d9c14936ffa0de1624d9b0178bd959925db41
JoaBories <joa.bories@artfx.fr>
2025-03-21 11:24:07 +0100

    [update] new variable on enemyData

--- Assets/Scripts/ScriptableObjects/EnemyData.cs
+++ Assets/Scripts/ScriptableObjects/EnemyData.cs
@@ -5,11 +5,15 @@ public class EnemyData : ScriptableObject
 {
     public bool isDropping;
     public float hp;
+    public float speed;
+    public float shootingRange;
 
-    public EnemyData(bool isLooting, float hp)
+    public EnemyData(bool isLooting, float hp, float speed, float shootingRange)
     {
         this.isDropping = isLooting;
         this.hp = hp;
+        this.speed = speed;
+        this.shootingRange = shootingRange;
     }
 }
 
